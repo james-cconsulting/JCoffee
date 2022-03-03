78c995460505fdf067d66ee5e05b42dc6185303d
james <james@campbellconsulting.co.uk>
2022-03-03 14:34:21 +0000

    fully working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,6 +30,32 @@ namespace JCoffee.Controllers
             return View();
         }
 
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+            // rows from db
+            var data = EmployeeProcessor.LoadEmployees();
+
+            // empty list to transfer rows to from db
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach (var row in data)
+            {   // iterate over the the list of employees (from db)
+                employees.Add(new EmployeeModel
+                { 
+                    // add the db values to new variables and add as row to empty list
+                    EmployeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    ConfirmEmail = row.EmailAddress
+                });
+            }
+
+            // return the list of employees to be used in view
+            return View(employees);
+        }
+
         //GET
         public ActionResult SignUp()
         {
